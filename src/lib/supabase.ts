b2f7107982d01e189d6f2a0de496bb873502fc11
Franklin Santaella <32347652+frank1411@users.noreply.github.com>
2025-05-25 20:53:16 -0400

    Actualizar configuración de Supabase

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
 
-// Reemplaza estos valores con tus credenciales de Supabase
 const supabaseUrl = 'https://ifffeanzsbjsdxrvvgme.supabase.co';
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlmZmZlYW56c2Jqc2R4cnZ2Z21lIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgyMTU2ODgsImV4cCI6MjA2Mzc5MTY4OH0.OBleByKL7Pe-dAW2-jIFrlQiZgIs-vJ9Kio0Hg1YzTk';
 
