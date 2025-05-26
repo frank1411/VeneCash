794d58ae9e63f13eef14d15564f998c167b7fa6b
Franklin Santaella <32347652+frank1411@users.noreply.github.com>
2025-05-25 20:51:46 -0400

    Configurar Supabase manualmente

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 // Reemplaza estos valores con tus credenciales de Supabase
-const supabaseUrl = 'https://[TU-PROYECTO-ID].supabase.co';
-const supabaseAnonKey = '[TU-ANON-KEY]';
+const supabaseUrl = 'https://ifffeanzsbjsdxrvvgme.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlmZmZlYW56c2Jqc2R4cnZ2Z21lIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgyMTU2ODgsImV4cCI6MjA2Mzc5MTY4OH0.OBleByKL7Pe-dAW2-jIFrlQiZgIs-vJ9Kio0Hg1YzTk';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
